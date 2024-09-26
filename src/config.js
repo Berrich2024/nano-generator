f89e298ad73f9f612ae32d0149a6e77aa8f879f0
Berrich2024 <sylvain.lagonotte.fr@gmail.com>
2024-09-26 17:00:07 +0200

    Delete src directory

--- src/config.js
+++
@@ -1,8 +0,0 @@
-// src/config.js
-const config = {
-    url: "www.freenanofaucet.com", // The website URL
-    refreshRate: 900, // Refresh rate in seconds
-    nanoAddress: "nano_134t6eihdxg9b5qreumpi5ok3qi7fxaerqwaheb663y983ec3j8biembx8ji" // Your Nano address
-};
-
-module.exports = config;
